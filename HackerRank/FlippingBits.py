fe7fc8eada226d941f3aee7e31aee7dd37aefa6c
Jonathan Ballard <asgardschosen@gmail.com>
2024-04-14 16:31:21 -0700

    finished Flipping Bits

--- HackerRank/FlippingBits.py
+++ HackerRank/FlippingBits.py
@@ -10,6 +10,37 @@
 #~ -----------------------------------------------------------------
 
 
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    # Write your code here
+    
+    # Bitwise NOT (flip bits) bitwise AND ((2^32) - 1) to ensure positives (since working with unsigned long)
+    return ~n & 2 ** 32 - 1
+
+if __name__ == '__main__':
+    
+    length = 3
+    n = [2147483647, 1, 0 ]
+
+    for el in n:
+        result = flippingBits(el)
+        print(result)
+
+
 
 
 
